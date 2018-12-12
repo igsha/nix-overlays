1e0c95df40575175df62ada8f52fc6495960829d
Igor Sharonov <igsha@users.noreply.github.com>
2018-12-12 22:04:42 +0300

    Add pandoc_2_4

--- default.nix
+++ default.nix
@@ -50,37 +50,59 @@ in rec {
 
   myGhc = pkgs.haskell.packages.ghc844.override {
     overrides = self: super: {
-      "haddock-library_1_6_1" = super.callPackage
-        ({ mkDerivation, base, base-compat, bytestring, containers, deepseq
-         , directory, filepath, hspec, hspec-discover, optparse-applicative
-         , QuickCheck, transformers, tree-diff
-         }:
-         mkDerivation {
-           pname = "haddock-library";
-           version = "1.6.1";
-           sha256 = "195fhaxr0c652icnd325jvzbi0anr2zlwyfjpl0dyn68v38apn5n";
-           libraryHaskellDepends = [
-             base bytestring containers deepseq transformers
-           ];
-           testHaskellDepends = [
-             base base-compat bytestring containers deepseq directory filepath
-             hspec optparse-applicative QuickCheck transformers tree-diff
-           ];
-           testToolDepends = [ hspec-discover ];
-           doHaddock = false;
-           description = "Library exposing some functionality of Haddock";
-           license = pkgs.stdenv.lib.licenses.bsd3;
-           hydraPlatforms = pkgs.stdenv.lib.platforms.none;
-         }) {};
-      /*pandoc = super.pandoc_2_3_1.override {
-        haddock-library = self.haddock-library_1_6_1;
-        hslua = super.hslua_1_0_1;
-        hslua-module-text = super.hslua-module-text_0_2_0.override { hslua = super.hslua_1_0_1; };
-      };*/
+      pandoc_2_4 = super.callPackage
+      ({ mkDerivation, aeson, aeson-pretty, base, base64-bytestring
+       , binary, blaze-html, blaze-markup, bytestring, Cabal
+       , case-insensitive, cmark-gfm, containers, criterion, data-default
+       , deepseq, Diff, directory, doctemplates, exceptions
+       , executable-path, filepath, Glob, haddock-library, hslua
+       , hslua-module-text, HsYAML, HTTP, http-client, http-client-tls
+       , http-types, JuicyPixels, mtl, network, network-uri, pandoc-types
+       , parsec, process, QuickCheck, random, safe, SHA, skylighting
+       , split, syb, tagsoup, tasty, tasty-golden, tasty-hunit
+       , tasty-quickcheck, temporary, texmath, text, time
+       , unicode-transforms, unix, unordered-containers, vector, weigh
+       , xml, zip-archive, zlib
+       }:
+       mkDerivation {
+         pname = "pandoc";
+         version = "2.4";
+         sha256 = "1kf1v7zfifh5i1hw5bwdbd78ncp946kx1s501c077vwzdzvcz2ck";
+         configureFlags = [ "-fhttps" "-f-trypandoc" ];
+         isLibrary = true;
+         isExecutable = true;
+         enableSeparateDataOutput = true;
+         setupHaskellDepends = [ base Cabal ];
+         libraryHaskellDepends = [
+           aeson aeson-pretty base base64-bytestring binary blaze-html
+           blaze-markup bytestring case-insensitive cmark-gfm containers
+           data-default deepseq directory doctemplates exceptions filepath
+           Glob haddock-library hslua hslua-module-text HsYAML HTTP
+           http-client http-client-tls http-types JuicyPixels mtl network
+           network-uri pandoc-types parsec process random safe SHA skylighting
+           split syb tagsoup temporary texmath text time unicode-transforms
+           unix unordered-containers vector xml zip-archive zlib
+         ];
+         executableHaskellDepends = [ base ];
+         testHaskellDepends = [
+           base base64-bytestring bytestring containers Diff directory
+           executable-path filepath Glob hslua pandoc-types process QuickCheck
+           tasty tasty-golden tasty-hunit tasty-quickcheck temporary text time
+           xml zip-archive
+         ];
+         benchmarkHaskellDepends = [
+           base bytestring containers criterion mtl text time weigh
+         ];
+         doCheck = false;
+         description = "Conversion between markup formats";
+         license = pkgs.stdenv.lib.licenses.gpl2;
+         hydraPlatforms = pkgs.stdenv.lib.platforms.none;
+       }) {};
+
       pandoc-crossref = super.pandoc-crossref.override { pandoc = self.pandoc; };
       pandoc-citeproc = super.pandoc-citeproc.override { pandoc = self.pandoc; };
       pandoc-include-code = super.pandoc-include-code.overrideAttrs (old: rec { doCheck = false; });
-      pandoc = super.pandoc_2_4.override {
+      pandoc = self.pandoc_2_4.override {
         haddock-library = super.haddock-library_1_7_0;
         hslua = super.hslua_1_0_1;
         hslua-module-text = super.hslua-module-text_0_2_0.override { hslua = super.hslua_1_0_1; };
