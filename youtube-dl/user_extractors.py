cc59e6939007ae8a71767542672f50aeb2fbf80e
Igor Sharonov <igsha@users.noreply.github.com>
2019-05-04 16:45:40 +0300

    Update youtube-dl extractor: move to hsl playlist

--- youtube-dl/user_extractors.py
+++ youtube-dl/user_extractors.py
@@ -1,5 +1,5 @@
 from .common import InfoExtractor
-import re
+import re, json
 
 class AnimediaIE(InfoExtractor):
     _VALID_URL = r'(?:https?://)?(?:www\.)?online.animedia.tv/anime/(?P<id>[-\w/]+)'
@@ -8,9 +8,16 @@ class AnimediaIE(InfoExtractor):
         video_id = self._match_id(url)
         webpage = self._download_webpage(url, video_id)
 
-        entry_id = self._html_search_regex(r'data-id=(?:"|\')(\d+)', webpage, name=video_id)
-        video_url_format = self._html_search_regex(r'<meta\s+property=(?:"|\')og:video(?:"|\')\s+href=(?:"|\')([^"\']+)', webpage, name=video_id)
-        video_url = video_url_format.format(entry_id=entry_id)
-        self.report_extraction(video_url)
+        groups = re.search(r'file:\s*"(https?://online.animedia.tv/embeds/playlist-j\.txt/[^"]+)",\s*plstart:\s*"([^"]+)"', webpage)
+        playlist, series = groups[1], groups[2]
 
-        return self.url_result(video_url, video_id=video_id, video_title=video_id.replace('/', '-'))
+        lst = json.loads(self._download_webpage(playlist, playlist))
+        for x in lst:
+            if x['id'] == series:
+                video_url = 'https:{}'.format(x['file'])
+                video_title = '{} - {}'.format(video_id, x['title'])
+                video_id = x['id']
+                break
+
+        self.report_extraction(video_url)
+        return {'id': video_id, 'title': video_title, 'url': video_url}
