bc1bb5a35e9cddd0844c346a72aa8daff87da601
Igor Sharonov <igsha@users.noreply.github.com>
2022-05-13 09:46:51 +0300

    Netron: 5.7.0 -> 5.7.8

--- pkgs/netron/default.nix
+++ pkgs/netron/default.nix
@@ -1,10 +1,10 @@
 { appimageTools, fetchurl }:
 
-appimageTools.wrapType2 {
+appimageTools.wrapType2 rec {
   pname = "Netron";
-  version = "5.7.0";
+  version = "5.7.8";
   src = fetchurl {
-    url = https://github.com/lutzroeder/netron/releases/download/v5.7.0/Netron-5.7.0.AppImage;
-    hash = "sha256-72qilYxqg2ihZvEwJNn9WeyMgGERMNMgoa+XQRgoP7M=";
+    url = "https://github.com/lutzroeder/netron/releases/download/v${version}/${pname}-${version}.AppImage";
+    hash = "sha256-2u0lem9umMKot/OSYcVNrilD1osd+CoZp9pLhKVO/OE=";
   };
 }
